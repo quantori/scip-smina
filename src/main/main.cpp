95d379f9102161f78217d91fed79ee2250e5a551
dkoes <dkoes@pitt.edu>
2021-01-29 07:30:47 -0500

    Remove PDBQT reference

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -852,10 +852,10 @@ Please report this error at http://smina.sf.net\n"
     * the type of computer system you are running it on,\n\
 	* all command line options,\n\
 	* configuration file (if used),\n\
-    * ligand file as PDBQT,\n\
-    * receptor file as PDBQT,\n\
-	* flexible side chains file as PDBQT (if used),\n\
-	* output file as PDBQT (if any),\n\
+    * ligand file,\n\
+    * receptor file,\n\
+	* flexible side chains file (if used),\n\
+	* output file  (if any),\n\
     * input (if possible),\n\
 	* random seed the program used (this is printed when the program starts).\n\
 \n\
@@ -920,9 +920,9 @@ Thank you!\n";
 		options_description inputs("Input");
 		inputs.add_options()
 		("receptor,r", value<std::string>(&rigid_name),
-				"rigid part of the receptor (PDBQT)")
+				"rigid part of the receptor")
 		("flex", value<std::string>(&flex_name),
-				"flexible side chains, if any (PDBQT)")
+				"flexible side chains, if any")
 		("ligand,l", value<std::vector<std::string> >(&ligand_names),
 				"ligand(s)")
 		("flexres", value<std::string>(&flex_res),
